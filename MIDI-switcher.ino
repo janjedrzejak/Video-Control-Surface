254f48847b6feab62dfccd4d258b166ff6696ade
Jan Jędrzejak <43744269+janjedrzejak@users.noreply.github.com>
2019-11-20 18:22:51 +0100

    Right potentiometer value after first plug in
    Device send potentiometer value after connecting via usb to computer. It is not necessary to setting potentiometer to initial value before turning on software to mixing video.

--- MIDI-switcher.ino
+++ MIDI-switcher.ino
@@ -14,7 +14,7 @@ const int buttonPin6 = 7;     //PREVIEW 2
 const int buttonPin7 = 8;     //PREVIEW 3
 const int buttonPin8 = 9;     //PREVIEW 4
 
-//initialize
+//initialize variables
 int buttonState1 = 0;         
 int buttonState2 = 0;
 int buttonState3 = 0;
@@ -25,6 +25,7 @@ int buttonState7 = 0;
 int buttonState8 = 0;
 int volume1 = 0;
 int volume1a = 0;
+bool initState=true;
 
 void setup() {
   Serial.begin(115200); //Set MIDI baud rate to communication
@@ -60,67 +61,50 @@ void loop() {
   //discovering button states
   if (buttonState1 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-    
     noteOn(0x90, 0x10, 0x45); //send midi as note on
-    
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
-
   if (buttonState2 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-    noteOn(0x90, 0x11, 0x45); //send midi as note on
-    
+    noteOn(0x90, 0x11, 0x45); //send midi as note on  
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
   if (buttonState3 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-    noteOn(0x90, 0x12, 0x45); //send midi as note on
-    
+    noteOn(0x90, 0x12, 0x45); //send midi as note on\   
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
   if (buttonState4 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-    noteOn(0x90, 0x13, 0x45); //send midi as note on
-    
+    noteOn(0x90, 0x13, 0x45); //send midi as note on   
   } else {
     digitalWrite(LED_BUILTIN, LOW); 
   }
   //------------------------------------------
   if (buttonState5 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-    noteOn(0x90, 0x20, 0x45); //send midi  as note on
-    
+    noteOn(0x90, 0x20, 0x45); //send midi  as note on  
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
   if (buttonState6 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-    noteOn(0x90, 0x21, 0x45); //send midi as note on
-    
+    noteOn(0x90, 0x21, 0x45); //send midi as note on    
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
   if (buttonState7 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-    noteOn(0x90, 0x22, 0x45); //send midi as note on
-    
+    noteOn(0x90, 0x22, 0x45); //send midi as note on    
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
   if (buttonState8 == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);
-
-    noteOn(0x90, 0x23, 0x45); //send midi as note on
-    
+    noteOn(0x90, 0x23, 0x45); //send midi as note on   
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
@@ -129,7 +113,11 @@ void loop() {
   volume1 = analogRead(A0) / 8; //read value
   delay(20); //wait 20ms
   volume1a = analogRead(A0) / 8; //read next value (for checking changes)
-  if (volume1!=volume1a) { //if change is discover send to serial command, if not, don't send
+  if(initState==true) { //if first connection send potentiometer value
+    noteOn(0xB0, 0x23, volume1a);
+    initState=false;
+  }
+  if (volume1!=volume1a) { //if change is discover send to serial command, if not, don't send midi
     noteOn(0xB0, 0x23, volume1a); //send midi as continues values
   }
 }
